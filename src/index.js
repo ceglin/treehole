75be0b3ec5df2eadd1d6315eae7aa56ee4e80fc6
chenglin <ceglin127@gmail.com>
2017-07-20 14:51:19 +0800

    'second-commit'

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@
 * @Author: ChengLin
 * @Date:   2017-07-20 11:35:55
 * @Last Modified by:   ChengLin
-* @Last Modified time: 2017-07-20 11:35:55
+* @Last Modified time: 2017-07-20 14:50:37
 */
 
 'use strict';
+
+function(){}
